399e8d2499696778501fac3ce678c2be8f0ce60e
iceval <valera.2001@mail.ru>
2022-07-09 07:05:12 +0300

    feat (TimesheetApi): create api models

---
+++ Timesheet.Api/Controllers/ReportController.cs
@@ -0,0 +1,34 @@
+﻿using AutoMapper;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Timesheet.Api.Models;
+using Timesheet.Domain;
+using Timesheet.Domain.Models;
+
+namespace Timesheet.Api.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ReportController : ControllerBase
+    {
+        private readonly IReportService _reportService;
+        private readonly IMapper _mapper;
+
+        public ReportController(IReportService reportService, IMapper mapper)
+        {
+            _reportService = reportService;
+            _mapper = mapper;
+        }
+
+        [HttpGet]
+        public ActionResult<GetEmployeeReportResponse> Get(string lastname)
+        {
+            var result = _reportService.GetEmployeeReport(lastname);
+
+            return _mapper.Map<GetEmployeeReportResponse>(result);
+        }
+    }
+}
